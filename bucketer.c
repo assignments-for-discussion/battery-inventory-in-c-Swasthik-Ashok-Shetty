b077e28e983512414c7912dfa5b0b6240a4e9029
Swasthik-Ashok-Shetty <91593888+Swasthik-Ashok-Shetty@users.noreply.github.com>
2023-12-03 17:41:24 +0530

    Update bucketer.c by Swasthik Ashok Shetty

--- bucketer.c
+++ bucketer.c
@@ -9,6 +9,29 @@ struct CountsBySoH {
 
 struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
   struct CountsBySoH counts = {0, 0, 0};
+    for(int i = 0; i < nBatteries; i++)
+    {
+      //Calculation of SoH of Batteries
+       float SoH = (((float)presentCapacities[i])/120.0)*100;
+
+      //Classification of Batteries based on SoH and count in each classification
+       if(SoH < 0.0 || SoH > 100.0)
+       { 
+          continue;
+       }
+       else if(SoH > 80.0)
+       {
+          counts.healthy++;
+       }
+       else if(SoH >= 62.0)
+       {
+          counts.exchange++;
+       }
+       else
+       {
+          counts.failed++;
+       }
+    }
   return counts;
 }
 
